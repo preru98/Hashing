dba8c77099d4cf375d9dfbcd395326ccfc2a345d
Prerna <prerna1998.ps@gmail.com>
2020-04-10 10:38:20 +0530

    Chaining | Collision Resolution

---
+++ chaining.cpp
@@ -0,0 +1,117 @@
+#include<iostream>
+using namespace std;
+
+const int size_hashtable=10; 
+struct Node{
+    int data;
+    Node* next;
+}*hashtable[size_hashtable];
+
+
+/*
+Chaining : Open Addressing method for collision resolution.
+
+*/
+void insert_in_hashtable(int[],int);
+        // Returns true if present and false otherwise
+int hash_function(int);
+Node* create_node(int);
+void insert(int, Node*);
+void view();
+bool search(int);  
+
+
+int main(){
+    int input_array[]={4,10,0,14,16,6,13,9}; //Inclusive Range 1-20 
+    int size_input_array=sizeof(input_array)/sizeof(input_array[0]);
+    insert_in_hashtable(input_array,size_input_array);
+    view();
+    cout<<search(17);
+    return 0;
+}
+
+void insert_in_hashtable(int input_array[], int size_input_array){
+    for(int i=0;i<size_hashtable;i++){     //Storing illegal values to hashtable
+        hashtable[i]=NULL;
+    }
+
+    for(int i=0;i<size_input_array;i++){   //Inserting elements
+        Node* new_node=create_node(input_array[i]);
+        int resultant_index=hash_function(input_array[i]); 
+        insert(resultant_index,new_node);
+    }
+}
+
+bool search(int key){
+    if(key>20){                //Out of range
+        return false;
+    }
+    int resultant_index=hash_function(key);
+    Node* traverse=hashtable[resultant_index];
+    while(traverse){
+        if(traverse->data>key){
+            return false;
+        }
+        if(traverse->data==key){
+            return true;
+        }
+        traverse=traverse->next;
+    }
+    return false;
+}
+
+int hash_function(int element){
+    return element%10;
+}
+
+Node* create_node(int element){
+    Node * new_node=new Node;
+    new_node->data=element;
+    new_node->next=NULL;
+    return new_node;
+}
+
+void insert(int start, Node * new_Node){
+    
+    if(!hashtable[start]){
+        hashtable[start]=new_Node;
+    }
+    else{
+        Node *traverse=hashtable[start],*tail=NULL;
+        if(traverse->data>new_Node->data){
+            new_Node->next=traverse;
+            hashtable[start]=new_Node;
+        }
+        else{
+            while(traverse){
+                if(traverse->data<new_Node->data){
+                    tail=traverse;
+                    traverse=traverse->next;
+                }
+                else{
+                    break;
+                }
+            }
+            if(traverse!=NULL){
+                tail->next=new_Node;
+                new_Node->next=traverse;
+            }
+            else{
+                tail->next=new_Node;
+            }
+        }
+    }
+}
+
+void view(){
+    Node* traverse;
+    for(int i=0;i<size_hashtable;i++){
+        cout<<"For i= "<<i<<"\n";
+        traverse=hashtable[i];
+        while(traverse){
+            cout<<traverse->data<<"\t";
+            traverse=traverse->next;
+        }
+        cout<<endl;
+    }
+}
